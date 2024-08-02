d23638695b7301c52470929daaf4d07c3c954c9e
Su6666666 <86417238+Su6666666@users.noreply.github.com>
2024-08-02 11:54:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from aiohttp import web
 from route import web_server
 import pyrogram.utils
 
-pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
+pyrogram.utils.MIN_CHANNEL_ID = -1001877309572
 
 
 class Bot(Client):
